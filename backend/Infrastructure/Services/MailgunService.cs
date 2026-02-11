d61b53f1ec9033edd17c9e5a3b8f12fd2abd0c5c
Penut3 <144951594+Penut3@users.noreply.github.com>
2026-02-11 22:27:46 +0100

    Updated domain values Mailgun service

--- backend/Infrastructure/Services/MailgunService.cs
+++ backend/Infrastructure/Services/MailgunService.cs
@@ -9,15 +9,17 @@ namespace Infrastructure.Services
     {
         private readonly string _mailgunUrl;
         private readonly string _email;
+        private readonly string _mailgunDomain;
 
         public MailgunService() {
             _mailgunUrl = Environment.GetEnvironmentVariable("MAILGUN_URL");
             _email = Environment.GetEnvironmentVariable("EMAIL");
+            _mailgunDomain = Environment.GetEnvironmentVariable("MAILGUN_DOMAIN");
         }
 
         public async Task<MailgunResultDto> SendAsync()
         {
-            var options = new RestClientOptions("https://api.mailgun.net")
+            var options = new RestClientOptions($"{_mailgunUrl}")
             {
                 Authenticator = new HttpBasicAuthenticator(
                     "api",
@@ -27,22 +29,12 @@ namespace Infrastructure.Services
 
             var client = new RestClient(options);
 
-
-            //var request = new RestRequest(
-            //    $"/v3/{_mailgunUrl}/messages",
-            //    Method.Post
-            //);
-
-            var request = new RestRequest("/v3/sandbox77b6cd64d51845daa1feb57e7d81349a.mailgun.org/messages", Method.Post);
-
+            var request = new RestRequest($"/v3/{_mailgunDomain}/messages", Method.Post);
             request.AlwaysMultipartFormData = true;
-            request.AddParameter(
-                 "from",
-                 "Mailgun Sandbox <postmaster@sandbox77b6cd64d51845daa1feb57e7d81349a.mailgun.org>"
-             );
+            request.AddParameter("from", $"Mailgun Sandbox <postmaster@{_mailgunDomain}>");
             request.AddParameter("to", $"S J <{_email}>");
             request.AddParameter("subject", "Hello S J");
-            request.AddParameter("text", "Congratulations...");
+            request.AddParameter("text", "Congratulations S J, you just sent an email with Mailgun! You are truly awesome!");
 
             var response = await client.ExecuteAsync(request);
 
@@ -53,5 +45,8 @@ namespace Infrastructure.Services
                 Message = response.Content
             };
         }
+
+
+
     }
 }
